3d065d3fa2c864fbd70f7c8a0e753454536476b8
nicekk <wk19920120@126.com>
2017-05-03 18:14:48 +0800

    新增了一个项目

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from . import models
+
+
+def index(request):
+    articles = models.Article.objects.all()
+    return render(request,'blog/index.html',{'articles':articles})
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request,'blog/article_page.html',{'article':article})
